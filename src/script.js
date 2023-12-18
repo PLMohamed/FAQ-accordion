3e455b3b7074528d321a806eb392c3b49f5a5139
Boumedine Mohamed Touati <141277277+PLMohamed@users.noreply.github.com>
2023-12-18 23:12:36 +0100

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-const questions = document.querySelectorAll(".title");
-
-questions.forEach(question => {
-    
-    question.addEventListener('click',() => {
-        question.parentElement.classList.toggle('opened');
-    })
-
-});
